51a546a919de798646ed32eb7defbd83399b1b0e
Vlatko Kosturjak <kost@linux.hr>
2020-01-02 07:47:38 +0100

    use ulx3s-build

--- build.sh
+++ build.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-export ulx3s_url=https://github.com/alpin3/ulx3s/releases/download/v2019.12.29/ulx3s-2019.12.29-linux-x86_64.tar.gz
 export ulx3s_dist=/dist/
 
- yum update -y ; \
- yum install -y python3 zip unzip make git && \
- curl -L $ulx3s_url | tar -xvz -C /opt -f - && \
- ln -sf /opt/ulx3s-* /opt/ulx3s && \
- mkdir -p /mt/scratch/tmp/openfpga/prjtrellis/libtrellis/ && \
- cd /opt/ulx3s/bin && \
- for i in ecp*; do ln -sf `pwd`/$i /mt/scratch/tmp/openfpga/prjtrellis/libtrellis/$i; done && \
- cd $HOME && \
  git clone https://github.com/emard/Next186.git && \
  cd Next186 && \
  unzip Next186_SoC_Diamond_Project.zip  && \
--- wrap.sh
+++ wrap.sh
@@ -6,5 +6,5 @@ if [ "x$DIAMOND_MAC" = "x" ]; then
 	echo "script will output in $HOME/dist"
 	exit 1
 fi
-docker run --rm --mac-address=$DIAMOND_MAC -it -e LM_LICENSE_FILE=/fpga/license.dat -v $HOME/fpga:/fpga -v $HOME/dist:/dist -v `pwd`:/scripts dok3r/diamond:v3.7 /scripts/build.sh
+docker run --rm --mac-address=$DIAMOND_MAC -it -e LM_LICENSE_FILE=/fpga/license.dat -v $HOME/fpga:/fpga -v $HOME/dist:/dist -v `pwd`:/scripts dok3r/ulx3s-build /scripts/build.sh
 
